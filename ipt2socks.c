5128beb7c172122ee1b0c983fb3493b0ba647df9
zfl9 <zfl9.com@gmail.com>
2019-12-01 21:51:25 +0800

    fix segmentation fault in tcp_write_cb

--- ipt2socks.c
+++ ipt2socks.c
@@ -20,7 +20,6 @@ enum {
     OPTION_IPV4    = 0x01 << 2, /* enable ipv4 */
     OPTION_IPV6    = 0x01 << 3, /* enable ipv6 */
     OPTION_DNAT    = 0x01 << 4, /* use REDIRECT instead of TPROXY (for tcp) */
-    OPTION_HFCLS   = 0x01 << 5, /* gracefully close the tcp connection pair */
     OPTION_DEFAULT = OPTION_TCP | OPTION_UDP | OPTION_IPV4 | OPTION_IPV6, /* default behavior */
 };
 
@@ -43,7 +42,7 @@ enum {
 #define BIND_PORT_DEFAULT 60080
 
 /* ipt2socks version string */
-#define IPT2SOCKS_VERSION "ipt2socks v1.0.2 <https://github.com/zfl9/ipt2socks>"
+#define IPT2SOCKS_VERSION "ipt2socks v1.0.3 <https://github.com/zfl9/ipt2socks>"
 
 /* tcp stream context typedef */
 typedef struct {
@@ -53,7 +52,6 @@ typedef struct {
     void       *socks5_buffer;
     uv_write_t *client_wrtreq;
     uv_write_t *socks5_wrtreq;
-    bool        is_half_close;
 } tcpcontext_t;
 
 /* function declaration in advance */
@@ -153,7 +151,6 @@ static void print_command_help(void) {
            " -c, --cache-size <size>            max size of udp lrucache, default: 256\n"
            " -f, --buffer-size <size>           buffer size of tcp socket, default: 8192\n"
            " -u, --run-user <user>              run the ipt2socks with the specified user\n"
-           " -G, --graceful                     gracefully close the tcp connection pair\n"
            " -R, --redirect                     use redirect instead of tproxy (for tcp)\n"
            " -T, --tcp-only                     listen tcp only, aka: disable udp proxy\n"
            " -U, --udp-only                     listen udp only, aka: disable tcp proxy\n"
@@ -167,7 +164,7 @@ static void print_command_help(void) {
 
 /* parsing command line arguments */
 static void parse_command_args(int argc, char* argv[]) {
-    const char *optstr = ":s:p:a:k:b:B:l:j:n:o:c:f:u:GRTU46vVh";
+    const char *optstr = ":s:p:a:k:b:B:l:j:n:o:c:f:u:RTU46vVh";
     const struct option options[] = {
         {"server-addr",   required_argument, NULL, 's'},
         {"server-port",   required_argument, NULL, 'p'},
@@ -182,7 +179,6 @@ static void parse_command_args(int argc, char* argv[]) {
         {"cache-size",    required_argument, NULL, 'c'},
         {"buffer-size",   required_argument, NULL, 'f'},
         {"run-user",      required_argument, NULL, 'u'},
-        {"graceful",      no_argument,       NULL, 'G'},
         {"redirect",      no_argument,       NULL, 'R'},
         {"tcp-only",      no_argument,       NULL, 'T'},
         {"udp-only",      no_argument,       NULL, 'U'},
@@ -305,9 +301,6 @@ static void parse_command_args(int argc, char* argv[]) {
                 break;
             case 'u':
                 run_as_user(optarg, argv);
-                break;
-            case 'G':
-                g_options |= OPTION_HFCLS;
                 break;
             case 'R':
                 g_options |= OPTION_DNAT;
@@ -438,7 +431,6 @@ int main(int argc, char* argv[]) {
     if (g_options & OPTION_TCP) LOGINF("[main] enable tcp transparent proxy");
     if (g_options & OPTION_UDP) LOGINF("[main] enable udp transparent proxy");
     if (g_options & OPTION_DNAT) LOGINF("[main] use redirect instead of tproxy");
-    if (g_options & OPTION_HFCLS) LOGINF("[main] gracefully close tcp connection");
     IF_VERBOSE LOGINF("[main] verbose mode (affect performance)");
 
     for (int i = 0; i < g_nthreads - 1; ++i) {
@@ -545,13 +537,12 @@ static void tcp_socket_listen_cb(uv_stream_t *listener, int status) {
     status = uv_accept(listener, (void *)client_stream);
     if (status < 0) {
         LOGERR("[tcp_socket_listen_cb] failed to accept tcp%c socket: (%d) %s", isipv4 ? '4' : '6', -status, uv_strerror(status));
-        uv_close((void *)client_stream, tcp_stream_close_cb);
+        uv_close((void *)client_stream, (void *)free);
         return;
     }
 
     int sockfd = -1;
     uv_fileno((void *)client_stream, &sockfd);
-    if (g_options & OPTION_HFCLS) set_keepalive(sockfd);
     skaddr6_t skaddr; char ipstr[IP6STRLEN]; portno_t portno;
 
     IF_VERBOSE {
@@ -566,7 +557,7 @@ static void tcp_socket_listen_cb(uv_stream_t *listener, int status) {
 
     if (g_options & OPTION_DNAT) {
         if (!(isipv4 ? get_tcp_origdstaddr4(sockfd, (void *)&skaddr) : get_tcp_origdstaddr6(sockfd, (void *)&skaddr))) {
-            uv_close((void *)client_stream, tcp_stream_close_cb);
+            uv_close((void *)client_stream, (void *)free);
             return;
         }
     } else {
@@ -592,14 +583,11 @@ static void tcp_socket_listen_cb(uv_stream_t *listener, int status) {
     status = uv_tcp_connect(connreq, socks5_stream, (void *)&g_server_skaddr, tcp_socks5_tcp_connect_cb);
     if (status < 0) {
         LOGERR("[tcp_socket_listen_cb] failed to connect to socks5 server: (%d) %s", -status, uv_strerror(status));
-        uv_close((void *)client_stream, tcp_stream_close_cb);
-        uv_close((void *)socks5_stream, tcp_stream_close_cb);
+        uv_close((void *)client_stream, (void *)free);
+        uv_close((void *)socks5_stream, (void *)free);
         free(connreq);
         return;
     }
-
-    uv_fileno((void *)socks5_stream, &sockfd);
-    if (g_options & OPTION_HFCLS) set_keepalive(sockfd);
 
     tcpcontext_t *context = malloc(sizeof(tcpcontext_t));
     context->client_stream = client_stream;
@@ -608,7 +596,6 @@ static void tcp_socket_listen_cb(uv_stream_t *listener, int status) {
     context->socks5_buffer = malloc(g_tcpbufsiz);
     context->client_wrtreq = malloc(sizeof(uv_write_t));
     context->socks5_wrtreq = malloc(sizeof(uv_write_t));
-    context->is_half_close = g_options & OPTION_HFCLS ? false : true;
     client_stream->data = context;
     socks5_stream->data = context;
 
@@ -826,25 +813,12 @@ static void tcp_stream_read_cb(uv_stream_t *selfstream, ssize_t nread, const uv_
     bool is_socks5_stream = (void *)selfstream == (void *)context->socks5_stream;
     uv_stream_t *peerstream = is_socks5_stream ? (void *)context->client_stream : (void *)context->socks5_stream;
 
-    if (nread == UV_EOF) {
-        if (context->is_half_close) {
-            IF_VERBOSE LOGINF("[tcp_stream_read_cb] tcp connection has been closed in both directions");
-            goto CLOSE_STREAM_PAIR;
-        } else {
-            int sockfd = -1;
-            uv_fileno((void *)peerstream, &sockfd);
-            if (shutdown(sockfd, SHUT_WR) < 0) {
-                LOGERR("[tcp_stream_read_cb] failed to send EOF to peer stream: (%d) %s", errno, errstring(errno));
-                goto CLOSE_STREAM_PAIR;
-            }
-            uv_read_stop(selfstream);
-            context->is_half_close = true;
-            return;
-        }
-    }
-
     if (nread < 0) {
-        LOGERR("[tcp_stream_read_cb] failed to read data from tcp stream: (%zd) %s", -nread, uv_strerror(nread));
+        if (nread == UV_EOF) {
+            IF_VERBOSE LOGINF("[tcp_stream_read_cb] tcp connection has been closed in both directions");
+        } else {
+            LOGERR("[tcp_stream_read_cb] failed to read data from tcp stream: (%zd) %s", -nread, uv_strerror(nread));
+        }
         goto CLOSE_STREAM_PAIR;
     }
 
@@ -876,6 +850,7 @@ static void tcp_stream_write_cb(uv_write_t *writereq, int status) {
 
     uv_stream_t *selfstream = writereq->handle;
     tcpcontext_t *context = selfstream->data;
+    if (!context) return; /* ignore it, otherwise it will cause a segmentation fault */
     bool is_socks5_stream = (void *)selfstream == (void *)context->socks5_stream;
     uv_stream_t *peerstream = is_socks5_stream ? (void *)context->client_stream : (void *)context->socks5_stream;
 
